56e970f113c2bc1b9cf40bc1ba1288df29c7b60c
Parth Ghodke <22901012parth@viva-technology.org>
2026-07-16 14:39:56 +0530

    Fix API instance name

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 import axios from "axios";
 
-const API = axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   withCredentials: true,
 });
